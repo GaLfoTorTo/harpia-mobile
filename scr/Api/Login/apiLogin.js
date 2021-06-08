7f3b6fd287f87b911a78167e5282d3fc6cdc0313
GaLfoTorTo <leonardovl117@gmail.com>
2021-06-08 17:24:40 -0300

    atualizacoes de auth

--- scr/Api/Login/apiLogin.js
+++ scr/Api/Login/apiLogin.js
@@ -2,18 +2,28 @@ import api from '../api';
 
 const handleLogin = async (email, password) => {
     const loginHTTP = await api
-        .post('logar', {
+        .post('logarApp', {
             email: email,
             password: password
         })
         .then(function(response){
-            console.warn('deu certo', response.data);
             return response
         })
         .catch(function(error){
-            console.warn('deuruim ', error);
+            return error
         });
     return loginHTTP
 }
 
 export default handleLogin;
+
+//method controller 
+
+/* public function logarApp(Request $request){
+        $credentials = $request->only('email', 'password');
+        if(Auth::attempt($credentials)){
+            $authentication = Auth::user();
+            return response()->json([$authentication]);
+        }
+    }
+ */
